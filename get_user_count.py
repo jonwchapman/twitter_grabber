40e879e4fe931854726149927fa23e0016670951
jon <jwchapman@ucdavis.edu>
2019-06-03 16:54:28 -0700

    Refactor

--- get_user_count.py
+++ get_user_count.py
@@ -1,3 +1,4 @@
+# primary use is to know how many times to loop if we are conducting some action against users within the database.
 def get_user_count():
     # limit 10 is present purely for debugging purposes
     stmt = "SELECT COUNT(*) FROM user limit 10"
