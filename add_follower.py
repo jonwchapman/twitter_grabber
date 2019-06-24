9f5bfa2db3d10a2b5db744f281c031d8413be4a3
jon <jwchapman@ucdavis.edu>
2019-06-24 00:07:19 -0700

    Added debug message for insertion of following relationship.

--- add_follower.py
+++ add_follower.py
@@ -3,13 +3,16 @@ from db_connection import db_connection
 
 def add_follower(follower_id, followee_id):
     mydb = db_connection()
-    stmt1 = "INSERT INTO follower (follower_id, followee_id) VALUES (%s, %s)"
+    print("Inserting Follower relationship")
+    print("follower " + follower_id)
+    print("followee " + followee_id)
+    stmt1 = "INSERT INTO follow (follower_id, followee_id) VALUES (%s, %s)"
 
     try:
         cursor = mydb.cursor()
         cursor.execute(stmt1, (follower_id, followee_id))
         mydb.commit()
     except:
-        print("")
+        print("Exception adding Follower Relationship")
 
     return 1
