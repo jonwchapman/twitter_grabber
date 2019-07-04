603524bbe442d512c3eb02028588b80d27807ded
jon <jwchapman@ucdavis.edu>
2019-07-03 23:41:27 -0700

    subprocess tweak in octo_grabber

--- octo_grabber.py
+++ octo_grabber.py
@@ -7,13 +7,14 @@ print("Octo-grabber")
 
 y = 0
 
-for x in range(0, 8800, 450):
+for x in range(0, 9000, 450):
     lower = x
     upper = x + 449
     print(lower)
     print(upper)
-    stmt = "python3 /home/turbo/PycharmProjects/twitter/follower_grabber.py " + str(lower) + " " + str(upper)
-    os.system(stmt)
-
+    stmt = "konsole -e python3 /home/turbo/PycharmProjects/twitter_grabber/follower_grabber.py " + str(lower) + " " + str(upper)
+    #stmt = "konsole -e python3 /home/turbo/PycharmProjects/twitter_grabber/test_octo.py"
+    #subprocess.run(['konsole', '--', 'python3', stmt])
+    child = subprocess.Popen(stmt, shell=True)
 
 
